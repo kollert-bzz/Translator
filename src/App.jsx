0556f361565bd57ed2ed6555422b8608810c019d
kollert-bzz <kollert@bzz.ch>
2024-09-04 22:43:04 +0200

    without api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React, { useRef  } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import './styles.css';
-
 
 export default function App() {
     const inputRef = useRef(null);
-    const solution = 'Erfahrung'
-    const solution_text = 'Die Lösung war: '
-    let counter = 0;
+    const [englishText, setEnglishText] = useState('');
+    const solution = 'Erfahrung';
+    const solutionText = 'Die Lösung war: ';
+    const [counter, setCounter] = useState(0);
+
 
 
     const handleKeyDown = (event) => {
@@ -16,20 +17,19 @@ export default function App() {
         }
     };
 
-
     const submitAnswer = () => {
         const inputElement = inputRef.current;
         const inputValue = inputElement.innerText.trim();
 
         if (inputValue) {
-            if (inputValue === solution){
-                wordGuessed()
+            if (inputValue === solution) {
+                wordGuessed();
             } else {
                 const heartImages = document.querySelectorAll('.hearts img');
                 if (heartImages.length > 0) {
                     heartImages[counter].classList.add('disappear');
-                    counter ++
-                    if (counter === 3){
+                    setCounter(prevCounter => prevCounter + 1);
+                    if (counter + 1 === 3) {
                         gameOver();
                     }
                 }
@@ -38,37 +38,34 @@ export default function App() {
     };
 
     const wordGuessed = () => {
-        const correct_screen = document.getElementById('third-sect');
-        correct_screen.classList.remove('disappear');
-        const cor_word = document.getElementById('cor-word');
-        cor_word.innerText = solution_text + solution;
-        //newExercise();
-        const countdown_element = document.getElementById('countdown');
-        let countdown = 3
+        const correctScreen = document.getElementById('third-sect');
+        correctScreen.classList.remove('disappear');
+        const corWord = document.getElementById('cor-word');
+        corWord.innerText = solutionText + solution;
+
+        const countdownElement = document.getElementById('countdown');
+        let countdown = 3;
         const intervalId = setInterval(() => {
-            countdown_element.innerText = countdown;
+            countdownElement.innerText = countdown;
             countdown--;
 
             if (countdown < 0) {
                 clearInterval(intervalId);
-                correct_screen.classList.add('disappear');
-                cor_word.innerText = '';
-                countdown_element.innerText = '4';
+                correctScreen.classList.add('disappear');
+                corWord.innerText = '';
+                countdownElement.innerText = '4';
             }
         }, 1000);
-    }
+    };
 
     const gameOver = () => {
         const inputElement = document.getElementById('translation-input');
         inputElement.contentEditable = "false";
-        const main_section = document.querySelector('#main-sect');
-        main_section.classList.add('disappear')
-        const sec_section = document.querySelector('#second-sect');
-        sec_section.classList.remove('disappear');
+        document.querySelector('#main-sect').classList.add('disappear');
+        document.querySelector('#second-sect').classList.remove('disappear');
         const fieldText = document.querySelector('#translation-input');
-        fieldText.innerHTML = ('');
-        const correct_screen = document.getElementById('third-sect');
-        correct_screen.classList.add('disappear');
+        fieldText.innerHTML = '';
+        document.getElementById('third-sect').classList.add('disappear');
         const heartImages = document.querySelectorAll('.hearts img');
         heartImages.forEach((image) => {
             image.classList.add('disappear');
@@ -76,13 +73,11 @@ export default function App() {
     };
 
     const restartGame = () => {
-        const main_section = document.querySelector('#main-sect');
-        main_section.classList.remove('disappear')
-        const sec_section = document.querySelector('#second-sect');
-        sec_section.classList.add('disappear');
-        counter = 0
+        document.querySelector('#main-sect').classList.remove('disappear');
+        document.querySelector('#second-sect').classList.add('disappear');
+        setCounter(0);
         const fieldText = document.querySelector('#translation-input');
-        fieldText.innerHTML = ('');
+        fieldText.innerHTML = '';
         fieldText.contentEditable = "true";
         const heartImages = document.querySelectorAll('.hearts img');
         heartImages.forEach((image) => {
@@ -90,31 +85,27 @@ export default function App() {
         });
     };
 
-
-
     return (
         <>
             <div className="logo">
-                <img src="Translator/img/logos/logo-no-background.png" alt="logo"/>
+                <img src="./img/logos/logo-no-background.png" alt="logo"/>
             </div>
             <div className="no-hearts">
-                <img src="/img/minecraft_no_heart.png" alt="Health"/>
-                <img src="/img/minecraft_no_heart.png" alt="Health"/>
-                <img src="/img/minecraft_no_heart.png" alt="Health"/>
+                <img src="./img/minecraft_no_heart.png" alt="Health"/>
+                <img src="./img/minecraft_no_heart.png" alt="Health"/>
+                <img src="./img/minecraft_no_heart.png" alt="Health"/>
             </div>
             <div className="hearts">
-                <img src="/img/minecraft_heart.png" alt="Health"/>
-                <img src="/img/minecraft_heart.png" alt="Health"/>
-                <img src="/img/minecraft_heart.png" alt="Healthhh"/>
+                <img src="./img/minecraft_heart.png" alt="Health"/>
+                <img src="./img/minecraft_heart.png" alt="Health"/>
+                <img src="./img/minecraft_heart.png" alt="Health"/>
             </div>
             <div id="score">
                 Score: / Highscore
             </div>
             <section id="main-sect">
                 <div className="text">
-                    <p id="en-text">
-                        Working for Andeo is a great experience
-                    </p>
+                    <p id="en-text">Working for Andeo is a great experience</p>
                     <p id="de-text">
                         Für Andeo zu arbeiten ist eine grossartige
                         <span
